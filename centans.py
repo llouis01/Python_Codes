9cb807992119320ff5b7c934bd5d56ecde917378
llouis01 <loubenslouis@live.com>
2020-05-25 17:54:56 -0400

    Updated centans
    1) updated script to Python 3.8 version
    2) updates needed were print functions, input(), and placement of certain variables

--- centans.py
+++ centans.py
@@ -9,32 +9,38 @@ from datetime import datetime
 from datetime import timedelta
 
 today = date.today() # date.var returns date
-print '\nToday is', (today.strftime('%B')), (today.strftime('%d')), (today.strftime('%Y')) # time formats used to return mo, day, yr
+print(f'\nToday is', (today.strftime('%B')), (today.strftime('%d')), (today.strftime('%Y'))) # time formats used to return mo, day, yr
 
-print "\nHello! What is your name?"
-user_name = raw_input('--> ')
-print '\nHow old are you, %s?' % user_name
-user_age = int(raw_input('--> '))
+print("\nHello! What is your name?")
+user_name = input('--> ')
+print(f'\nHow old are you, {user_name}?')
+user_age = int(input('--> '))
 
 # Python lists start with 0 index so add in a dummy for 0
 months = ['Dummy', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-print '\nWhat day, month and year were you born?'
-user_day = int(raw_input('Day: ')) # use this line to get index from months list
-user_month = months.index(raw_input('Month: '))
-user_year = int(raw_input('Year: ')) 
+print('\nWhat day, month and year were you born?')
+user_day = int(input('Day: ')) # use this line to get index from months list
+user_month = months.index(input('Month: '))
+user_year = int(input('Year: ')) 
 date_birth = user_year, user_month, user_day
 
 
 
-print '\nYou were born on', date_birth
+print(f'\nYou were born on {months[user_month]} {user_day}th {user_year}.')
 
 if today.month > user_month or (today.month == user_month and today.day >= user_day):
     NYE_birth = (today.year - user_age, 1, 1)
 else:
     NYE_birth = (today.year - user_age - 1, 1, 1) # use this conditional because past january, the program counts everything as a year.
 
-print 'The New Year\'s Eve of that year was ', NYE_birth
-print 'You were born', user_month - 1, 'months and', user_day - 1, 'days after NYE.\n'
+print(f'The New Year\'s Eve of that year was January 1st {user_year}')
+print(f'You were born {user_month - 1} month(s) and, {user_day - 1} days after NYE.')
+
+
+if (user_month == (today.strftime('%B')) and user_day == (today.strftime('%d'))):
+    print(f'Happy Birthday, {user_name}!')
+else:
+    print('\n')
 
 # need to include months
 
@@ -43,5 +49,5 @@ if user_month < today.month or (today.month == user_month and today.day <= user_
 else:
     ry = 100 - user_age - 1
 age_cent = date (int(today.year + ry), int(user_month), int(user_day))
-print '\nWow, a long way to go! You will turn 100 years-old on',  age_cent
-print '\n'
+print(f'Wow, a long way to go! You will turn 100 years-old on  {age_cent}')
+print('\n')
