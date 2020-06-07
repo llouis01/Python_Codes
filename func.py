37ab24fe6fffde60ac920ffebe5ccea9107a73dc
llouis01 <loubenslouis@live.com>
2020-06-07 02:27:08 -0400

    Updating func.py
    1) putting numpy and pandas together to work on datasets

--- func.py
+++ func.py
@@ -4,27 +4,34 @@ import numpy as np
 
 import pandas as pd
 
-num1 = input("Enter first number: ")
-num2 = input("Second one: ")
+htwt = np.array(pd.read_csv("C:\\Users\\Loube\\Desktop\\htwt.csv")) # quote and \\ to import file
+print(htwt[htwt[0]=="Male"])
 
-def addNum(x, y):
-    if x > y:
-        print("First is greater")
-    elif x < y:
-        print("First is less")
-    else:
-        print("They must be equal")
-    
-addNum(num1, num2)
-
+num1, num2, num3, num4, num5 = input("Enter 5 numbers: ").split() # must convert the entry to int
 print()
 
-print("Basic analysis of", num1, "and", num2)
+print("*** Basic analysis of your numbers ***")
+print()
 
-x = [num1, num2]
+if num1 > num5:
+    print("First is greater than last entry")
+elif num1 < num5:
+    print("Last entry is greater than first")
+else:
+    print("They must be equal")
+           
+    
+print("The sum of your numbers is ", int(num1) + int(num2) + int(num3) + int(num4) + int(num5))
+print()
 
-np_nums = np.array(x)
+np_nums = np.array([num1, num2, num3, num4, num5]) # put input into an array
 
-print() # next line
+print("Your numbers have been transformed into a ", type(np_nums)," data frame.")
 
-print(type(np_nums))
+print(np_nums)
+
+print("The mean of your numbers is ", np.mean(np_nums))
+
+print("The median is ", np.median(np_nums))
+
+print("The correlation is random, so do not expect any meaning from ", np.corrcoef(np_nums))
