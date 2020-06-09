c95038be9470ee10bc08a021ec19cf2343f0cef5
llouis01 <loubenslouis@live.com>
2020-06-09 12:18:04 -0400

    updating plt.py
    1) adding titles and labels to the histogram and line plot charts.
    
    2) also changed color of graphs

--- plt.py
+++ plt.py
@@ -1,16 +1,29 @@
-# working with matplotlib
+# Data Visualization with matplotlib
+
 import matplotlib.pyplot as plt
 
 import numpy as np
 
-s = np.random.normal(0, 10, 100)
-x = [1991, 1995, 1999, 2004, 2009]
+s = np.random.normal(0, 2, 100)
+x = ['1991', '1995', '1999', '2004', '2009']
 y = [14.5, 17.9, 21.3, 31.2, 45.8]
 
 print(s)
 
-plt.plot(x, y, color = 'turquoise') #.plt is better with dates on the x-axis
+plt.plot(x, y, color = 'purple') #.plt is better with dates on the x-axis; better for two variables generally
+plt.xlabel('Year')
+plt.ylabel('Median Salary')
+plt.title('Median Salary in the US from 1991 to 2009')
 plt.show() # why do I get all the decimals from y
+plt.clf() # cleans up screen after displaying above hostogram
 
 plt.hist(s, bins=10, color = 'purple') # bins to break up the data into appropriate boundaries
+plt.xlabel('Random Output')
+plt.ylabel('Range Composition')
+plt.title('Histogram of a random distribution')
 plt.show()
+plt.clf() # cleans up screen after displaying above hostogram
+
+print()
+
+print(min(s), max(s))
