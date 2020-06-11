d5a2ccf7ebe28980a8ae530b398d9492ebcfdc2e
llouis01 <loubenslouis@live.com>
2020-06-10 22:25:33 -0400

    updating dict.py
    1) objects within a dictionary cannot be duplicate and are immutable

--- dict.py
+++ dict.py
@@ -18,4 +18,16 @@ print("With keys:", ctr_pop.keys())
 print()
 print("And values:", ctr_pop.values())
 print()
-print("Population of Zoolandia is", ctr_pop['Zoolandia'], "million.") # key in brackets yields value
+print("Population of Zoolandia is", ctr_pop['Zoolandia'], "million.") # var[key]=key_val
+print()
+
+"""
+dictionaries cannot contain dupplicates and contents are immutable objects.
+if I try to include another key that already exists, the value will be up-
+dated but the additional key will not be included.
+"""
+
+ctr_pop = {'Haiti' : 11.2, 'Zoolandia' : 32.4, 'Wakanda' : 333.1,
+           'Diagon Alley' : 0.002, 'Zoolandia' : 39.3}
+
+print(ctr_pop) # the last value entered for the preexisting key will be kept
