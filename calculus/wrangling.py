e40c7e64ad3d8e84e86fa8619b0c0fb4e5b93a00
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-12-02 12:31:52 -0500

    update for memory
    files were too big; moved to off-github folder and consolidated notes.docx and README.md into one README.md document instead.

--- calculus/wrangling.py
+++ calculus/wrangling.py
@@ -6,5 +6,12 @@ Created on Wed Dec  2 12:08:11 2020
 """
 
 import pandas as pd
+import os
 
-data = pd.read_csv('FY17.csv')
+##### find files in this directory
+os.chdir('C:\\Users\\Loube\\Documents\\FY_files')
+
+##### import and cleanse data
+data = pd.read_csv('FY17.csv') 
+
+##### 
