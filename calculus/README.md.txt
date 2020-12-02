b49eb9829f6d3ef08177c033347209036bdaab14
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-12-02 12:26:12 -0500

    Side project
    This side project is about wrangling some quantitative data using pandas. I will write the script for one file and run all five through the script. The data is some financial data.

---
+++ calculus/README.md.txt
@@ -0,0 +1,3 @@
+Just some side projects.
+
+FY17: No dob value or column.
---
+++ calculus/wrangling.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 12:08:11 2020
+
+@author: Loube
+"""
+
+import pandas as pd
+
+data = pd.read_csv('FY17.csv')
