87f6f6afead779288d9dd78e092dda948f0fff75
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-12-03 13:37:37 -0500

    updated variables
    to avoid confusion, I conventionally named the variables.

--- calculus/wrangling.py
+++ calculus/wrangling.py
@@ -16,14 +16,14 @@ os.chdir('C:\\Users\\Loube\\Documents\\FY_files')
 data = pd.read_csv('FY17.csv', low_memory=False)
 
 # filter down by doj's favorite child
-child2 = data[data['parent_award_agency_name']=='FEDERAL BUREAU OF INVESTIGATION'].copy()
-child1 = child2[child2['awarding_sub_agency_name']=='FEDERAL BUREAU OF INVESTIGATION'].copy()
+child1 = data[data['parent_award_agency_name']=='FEDERAL BUREAU OF INVESTIGATION'].copy()
+child2 = child1[child1['awarding_sub_agency_name']=='FEDERAL BUREAU OF INVESTIGATION'].copy()
 
 # clears white space
-child1['funding_office_name'].replace({' +':' '}, regex=True, inplace = True)
+child2['funding_office_name'].replace({' +':' '}, regex=True, inplace = True)
 
 # consolidate all field offices into one entity
-child1['funding_office_name'].replace({r'.*FIELD OFFICE':'FIELD OFFICES'},
+child2['funding_office_name'].replace({r'.*FIELD OFFICE':'FIELD OFFICES'},
                                       regex=True, inplace = True)
 
 # pull out these columns for new data set
@@ -35,5 +35,8 @@ col_names = ['contract_transaction_unique_key', 'contract_award_unique_key',
              'period_of_performance_current_end_date', 'awarding_office_name',
              'funding_office_name', 'recipient_name']
 
-child1 = child1[col_names].copy()
+# new df with only fbi data and fewer columns
+child3 = child1[col_names].copy()
 
+# change name of certain columns
+child3.rename(columns={''})
